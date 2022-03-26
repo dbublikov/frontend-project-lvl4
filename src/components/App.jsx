6d5364316deb07416b432b90ba635fa8cfc3d068
dbublikov <bublikow.dmitri@gmail.com>
2022-03-26 13:47:44 +0300

    add react

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => (
+  <div className="card text-center">
+    <div className="card-body">
+      <h5 className="card-title">Chat (Slack)</h5>
+      <p className="card-text">Coming up soon...</p>
+    </div>
+  </div>
+);
+
+export default App;
