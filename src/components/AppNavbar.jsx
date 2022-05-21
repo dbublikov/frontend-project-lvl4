97e12909258c990239f73f1a9c96a8fe28031e14
dbublikov <bublikow.dmitri@gmail.com>
2022-05-21 12:42:56 +0300

    refactoring AppNavbar

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -3,25 +3,40 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../hooks/index.js';
 
-function AuthButton() {
+const AuthSection = () => {
   const auth = useAuth();
+  const getUsername = () => JSON.parse(localStorage.getItem('userId')).username;
+
+  const renderSection = () => {
+    if (auth.loggedIn) {
+      return (
+        <>
+          <Navbar.Text>
+            <b>{getUsername()}</b>
+            &nbsp; |
+          </Navbar.Text>
+          <Nav.Link onClick={auth.logOut}>Log Out</Nav.Link>
+        </>
+      );
+    }
+
+    return <Button as={Link} to="/login">Log In</Button>;
+  };
 
   return (
-    auth.loggedIn
-      ? <Button onClick={auth.logOut}>Log Out</Button>
-      : <Button as={Link} to="/login">Log In</Button>
+    <Navbar.Collapse>
+      {renderSection()}
+    </Navbar.Collapse>
   );
-}
+};
 
-function AppNavbar() {
-  return (
-    <Navbar className="mb-3" bg="light" expand="lg">
-      <Link to="/" className="mr-auto navbar-brand">Home</Link>
-      <Nav>
-        <AuthButton />
-      </Nav>
-    </Navbar>
-  );
-}
+const AppNavbar = () => (
+  <Navbar className="mb-3" bg="light">
+    <Link to="/" className="mr-auto navbar-brand">Home</Link>
+    <Nav>
+      <AuthSection />
+    </Nav>
+  </Navbar>
+);
 
 export default AppNavbar;
